9a23a8508ece59151992c9879419ce65bb9b7868
Ron Tadeo <ronaldo.tadeo@globalworkforce.com>
2011-11-09 13:50:52 +0800

    implemented randomness change

--- movie_plot.php
+++ movie_plot.php
@@ -45,7 +45,7 @@ class MoviePlotGenerator {
 		$plot  = "He's a $his_adjective $his_occupation with a $his_quirk. ";
 		$plot .= "She's a $her_adjective $her_occupation with a $her_quirk. ";
 		$plot .= "They fight crime.";
-		$plot .= "\n\nFeature 1: hey its feature One.";
+		
 		return $plot;
 	}
 	
@@ -58,7 +58,7 @@ class MoviePlotGenerator {
 	 */	
 	protected function getRandom($arr)
 	{
-		return $arr[rand(0, count($arr) - 1)];
+		return $arr[mt_rand(0, count($arr) - 1)];
 	}
 		
 }
